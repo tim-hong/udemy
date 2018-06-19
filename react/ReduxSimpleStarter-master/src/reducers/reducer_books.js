8ede20f503959fb2821a1be44b09169486282a93
tim-hong <timothy.d.hong@gmail.com>
2018-06-18 23:26:02 -0700

    latest changes

---
+++ react/ReduxSimpleStarter-master/src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        {title: 'Javascript: The Good Parts'},
+        {title: 'Harry PPotter'},
+        {title: 'The Dark Tower'},
+        {title: 'Eloquent Ruby'},
+        {title: 'The book'},
+    ]
+}
--- react/ReduxSimpleStarter/src/index.js
+++ react/ReduxSimpleStarter/src/index.js
@@ -1,18 +1,46 @@
-import React from 'react';
+import React, {Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import _ from 'lodash';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-const API_KEY = 'AIzaSyC2nNNNxUrRQXXMgaBi6d_jXIGeyqFDv90';
+const API_KEY = 'AIzaSyDLhbTF0rN31acy1fgY3IXcM6yHfioEc9w';
 
-// Create a new component.
-// This component should produce some HTML
-const App = () => {
-    return (
-    <div>
-        <SearchBar />
-    </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+        this.videoSearch('surfboards');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term:term}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+
+    render() {
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
+        return (
+        <div>
+            <SearchBar onSearchTermChange={videoSearch}/>
+            <VideoDetail video={this.state.selectedVideo}/>
+            <VideoList
+                onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                videos={this.state.videos} />
+        </div>
+        );
+    }
 }
 
 // Take this component's generated HTML and put it
